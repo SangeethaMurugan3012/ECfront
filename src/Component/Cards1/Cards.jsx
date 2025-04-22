8538ffde6605ef7e136bf267faa3f85b9d9a5836
SangeethaMurugan3012 <sangeethamurugan3012@gmail.com>
2025-04-22 15:00:14 +0530

    ECfront

--- src/Component/Cards1/Cards.jsx
+++ src/Component/Cards1/Cards.jsx
@@ -33,8 +33,8 @@ export default function Cards() {
         <div className="col ">
           <div className="card item1 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header'>
-            <img src={img1} className="card-img-top bottom"  />
-            <img src={img11} className="card-img-top top" />
+            <img src={img1}  alt='this img not avilable' className="card-img-top bottom"  />
+            <img src={img11} alt='this img not avilable'  className="card-img-top top" />
             </div>
             <div className="card-body cardbody1">
               <h5 className="card-title">Strppy Crop <br /> top</h5>
@@ -46,8 +46,8 @@ export default function Cards() {
         <div className="col ">
           <div className="card item2 mt-lg-5 " style={{ width: "14rem" }}>
             <div className='card-header'>
-            <img src={img2} className="card-img-top bottom" />
-            <img src={img12} className="card-img-top top"  />
+            <img src={img2}  alt='this img not avilable' className="card-img-top bottom" />
+            <img src={img12} alt='this img not avilable'  className="card-img-top top"  />
             </div>
             <div className="card-body cardbody2">
               <h5 className="card-title">Flat sandals with ankle strap</h5>
@@ -60,8 +60,8 @@ export default function Cards() {
         <div className="col">
           <div className="card item3 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header'>
-              <img src={img3} className="card-img-top bottom"  />
-              <img src={img13} className="card-img-top top"  />
+              <img src={img3}  alt='this img not avilable' className="card-img-top bottom"  />
+              <img src={img13} alt='this img not avilable'  className="card-img-top top"  />
             </div>
             <div className="card-body cardbody3">
               <h5 className="card-title">Printed tank <br /> top</h5>
@@ -74,8 +74,8 @@ export default function Cards() {
         <div className="col ">
           <div className="card item4 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header'>
-            <img src={img4} className="card-img-top bottom"  />
-            <img src={img14} className="card-img-top top" />
+            <img src={img4}  alt='this img not avilable' className="card-img-top bottom"  />
+            <img src={img14} alt='this img not avilable'  className="card-img-top top" />
             </div>
             <div className="card-body cardbody4">
               <h5 className="card-title">Cotton Cargo Bermuda shorts</h5>
@@ -88,8 +88,8 @@ export default function Cards() {
         <div className="col ">
           <div className="card item5 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header' >
-            <img src={img5} className="card-img-top bottom"  />
-            <img src={img15} className="card-img-top top" />
+            <img src={img5}  alt='this img not avilable' className="card-img-top bottom"  />
+            <img src={img15} alt='this img not avilable'  className="card-img-top top" />
             </div>
             <div className="card-body cardbody5">
               <h5 className="card-title">Rustic Strappy Crop top with</h5>
@@ -105,8 +105,8 @@ export default function Cards() {
         <div className="col">
           <div className="card item6 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header'>
-            <img src={img6} className="card-img-top bottom" />
-            <img src={img16} className="card-img-top top"  />
+            <img src={img6}  alt='this img not avilable' className="card-img-top bottom" />
+            <img src={img16} alt='this img not avilable'  className="card-img-top top"  />
             </div>
             <div className="card-body cardbody6">
               <h5 className="card-title">Mini dress with ruffled strap</h5>
@@ -117,8 +117,8 @@ export default function Cards() {
         <div className="col">
           <div className="card item7 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header'>
-            <img src={img7} className="card-img-top bottom" />
-            <img src={img17} className="card-img-top top" />
+            <img src={img7}  alt='this img not avilable' className="card-img-top bottom" />
+            <img src={img17} alt='this img not avilable'  className="card-img-top top" />
             </div>
             <div className="card-body cardbody7">
               <h5 className="card-title">Contrast trainers with mesh</h5>
@@ -131,8 +131,8 @@ export default function Cards() {
         <div className="col">
           <div className="card item8 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header'></div>
-            <img src={img8} className="card-img-top bottom" />
-            <img src={img18} className="card-img-top top"/>
+            <img src={img8}  alt='this img not avilable' className="card-img-top bottom" />
+            <img src={img18} alt='this img not avilable'  className="card-img-top top"/>
             <div className="card-body cardbody8">
               <h5 className="card-title">Linen blend waistcoat</h5>
               <p className="card-text">$13.90</p>
@@ -144,8 +144,8 @@ export default function Cards() {
         <div className="col">
           <div className="card item9 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header'></div>
-            <img src={img9} className="card-img-top bottom" />
-            <img src={img19} className="card-img-top top" />
+            <img src={img9}  alt='this img not avilable' className="card-img-top bottom" />
+            <img src={img19} alt='this img not avilable'  className="card-img-top top" />
             <div className="card-body cardbody9">
               <h5 className="card-title">Denim bag with fringing</h5>
               <p className="card-text">$23.90</p>
@@ -157,8 +157,8 @@ export default function Cards() {
         <div className="col">
           <div className="card item10 mt-lg-5" style={{ width: "14rem" }}>
             <div className='card-header'>
-            <img src={img10} className="card-img-top bottom"  />
-            <img src={img20} className="card-img-top top"/>
+            <img src={img10} alt='this img not avilable'  className="card-img-top bottom"  />
+            <img src={img20} alt='this img not avilable'  className="card-img-top top"/>
             </div>
             <div className="card-body cardbody10">
               <h5 className="card-title">Striped knit bandeau crop top</h5>
